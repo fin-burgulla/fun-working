9a8cd4898a216902625009ecb587a94e0b5f3285
Serafin Burgulla <burgullasj@gmail.com>
2025-09-10 03:58:49 +0000

    comments

--- funny_program.py
+++ funny_program.py
@@ -1,6 +1,9 @@
+# Print a funny question to start the program
 print("Why did the Python programmer wear glasses?")
+# Print the punchline
 print("Because they couldn't C!")
 
 # ...existing code...
 
+# Print a closing funny message
 print("That's all folks! Don't forget to laugh at your own code errors!")
