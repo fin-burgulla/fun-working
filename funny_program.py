c4b2649dbc01297045e39976a5c6427779aac56e
Serafin Burgulla <burgullasj@gmail.com>
2025-09-10 03:58:14 +0000

    print

---
+++ funny_program.py
@@ -0,0 +1,6 @@
+print("Why did the Python programmer wear glasses?")
+print("Because they couldn't C!")
+
+# ...existing code...
+
+print("That's all folks! Don't forget to laugh at your own code errors!")
